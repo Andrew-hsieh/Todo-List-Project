46c1c435fcb08dc4b1aae5ac6517b1535f8b06f3
zhi999xie <zhi999xie@gmail.com>
2021-03-08 13:30:18 +1100

    UI Set Up - 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 import "./styles.scss";
-/*
-<div class="todo">
-    <li></li>
-     <button></button>
-</div>
-                */
+
+/* Selectors
+----------------------
+*/
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+
+
+
+/* Event listeners
+----------------------
+*/
+todoButton.addEventListener('click', addTodo);
+
+
+
+
+/* Functions
+----------------------
+*/
+function addTodo(e){
+    e.preventDefault();
+    //Create Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    
+    //Create LI
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('.todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton);
+    //Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('trash-btn')
+    todoDiv.appendChild(trashButton);
+
+    //Append to list
+    todoList.appendChild(todoDiv)
+
+    //clear
+    todoInput.value='';
+}
+
+
+
